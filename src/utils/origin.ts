e1da349853177f918fab32b0eafb9ee22358f09b
davidvanleeuwen <davz0rs@gmail.com>
2023-11-08 13:07:03 +0100

    Hotfix dist folder

--- src/utils/origin.ts
+++ src/utils/origin.ts
@@ -1,7 +1,7 @@
 export const potentialDistFolder = () => {
   if (!document) return '';
   const host = new URL(import.meta.url).host;
-  if (['cdn.jsdelivr.net', 'cdn.mave.io'].includes(host)) {
+  if (['cdn.jsdelivr.net', 'cdn.mave.io', 'cdn.video-dns.com'].includes(host)) {
     return 'dist/';
   }
   return '';
