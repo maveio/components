2e4bad3de0eb8e237d426b99f1f840c1ae75855c
wietsehage <wietse@wietsehage.nl>
2023-12-04 14:57:37 +0100

    Explicit aspect ratio for children elements

--- src/themes/dolphin.ts
+++ src/themes/dolphin.ts
@@ -19,6 +19,7 @@ export function build(name, LitElement, html, css) {
         display: flex;
         width: 100%;
         height: 100%;
+        aspect-ratio: var(--aspect-ratio, 16 / 9);
         object-fit: contain;
         display: flex !important;
         background: transparent;
@@ -30,6 +31,7 @@ export function build(name, LitElement, html, css) {
         display: flex;
         width: 100%;
         height: 100%;
+        aspect-ratio: var(--aspect-ratio, 16 / 9);
         background: transparent;
         --media-font-family: 'Inter', system-ui, sans-serif;
         --media-background-color: transparent;
