6e56afc508ca47953b04abe2f224cdf310720b78
David <hi@davidvanleeuwen.nl>
2026-06-30 18:20:37 +0200

    Hotfix: +esm theme loading failure

--- src/utils/origin.ts
+++ src/utils/origin.ts
@@ -2,8 +2,13 @@ export function potentialDistFolder(): string | null {
   if (typeof document === 'undefined') return null;
 
   const pathname = new URL(import.meta.url).pathname;
+
   if (pathname.includes('+esm')) {
     return 'dist/';
+  }
+
+  if (pathname.includes('/npm/@maveio/components') && pathname.includes('/dist/')) {
+    return './';
   }
 
   return null;
